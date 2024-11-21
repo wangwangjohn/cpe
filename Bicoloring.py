25197f133259e699ad5641baac1223949a5f2b98
pussyjohn123 <john555123456@gmail.com>
2024-11-21 21:10:33 +0800

    11/21

--- Bicoloring.py
+++ Bicoloring.py
@@ -1,37 +1,33 @@
+# 深度搜尋法
 def dfs(node, color, graph, colors):
-    # 檢查節點是否已經染色，若已染色，則確認顏色是否符合
+    # 檢查染色，如果節點已染色過需檢查是否與當前遞迴要染的顏色一樣
     if colors[node] != 0:
         return colors[node] == color
-
-    # 如果節點未染色過，將節點染色
+    # 染色
     colors[node] = color
-
-    # 遍歷所有相鄰節點
-    for neighbor in graph[node]:
-        # 若相鄰節點無法成功染色，則返回 False
-        if not dfs(neighbor, -color, graph, colors):
+    # 對當前的節點進行動作
+    for i in graph[node]:
+        if not dfs(i, -color, graph, colors):
             return False
-
     return True
 
 
 while True:
-    # n 節點數 m 邊數
+    # n為節點數，m為邊數
     n = int(input())
     if n == 0:
         break
     m = int(input())
-
     # 初始化鄰接表，每個節點對應一個空列表
     graph = [[] for _ in range(n)]
+    # 因為是強連通所以新增進鄰接表
     for _ in range(m):
-        a, b = map(int, input().split(" "))
+        a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
-
-    # 初始化顏色陣列，0表示未染色
-    colors = [0] * n
-    if dfs(0, 1, graph, colors):
+    # 顏色陣列，0代表尚未染色
+    colors = [0]*n
+    if dfs(0, 1, graph, colors) == True:
         print("BICOLORABLE.")
     else:
         print("NOT BICOLORABLE.")
