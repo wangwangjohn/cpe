155ff66347382bc8efb51dbc21615beedeb70e12
pussyjohn123 <john555123456@gmail.com>
2025-03-18 22:35:49 +0800

    03/18練習

--- TeX Quotes.py
+++ TeX Quotes.py
@@ -1,21 +1,14 @@
-while True:
+count = 0
+while (True):
     try:
-        n = input()
+        n = list(input())
+        for i in range(len(n)):
+            if n[i] == '"':
+                if count % 2 == 0:
+                    n[i] = "``"
+                else:
+                    n[i] = "''"
+                count += 1
+        print(''.join(n))
     except EOFError:
         break
-    c = 0
-    a = []
-    # 尋訪字串每個字元
-    for i in n:
-       # 當尋訪到"時候以c判斷當前"是否為第一個"
-        if i == "\"":
-            if c == 0:
-                a.append("``")
-                c = 1
-            else:
-                a.append("''")
-                c = 0
-        else:
-            a.append(i)
-    result = "".join(a)
-    print(result)
